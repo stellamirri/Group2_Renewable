0e67130822e85bd52f344b980789781d6efe1636
Stella Mirri <stella.mirri@etu.minesparis.psl.eu>
2026-05-21 12:10:31 +0200

    Addition of the Spanish evolution in hydro generation

--- Group2_2026/Hydro_gen_FR.py
+++ Group2_2026/Hydro_gen_FR.py
@@ -16,6 +16,7 @@ params1 = {
     "end": "2025-01-01T00:00:00.000Z",
     "temporalGranularity": "yearly"
 }
+
 params2 = {
     "zone": "FR",
     "start": "2015-01-01T00:00:00.000Z",
@@ -24,30 +25,69 @@ params2 = {
 }
 #there is a 10 years time limit to this API command and there is no data before 2015 and no data after 2024
 
+
+params3 = {
+    "zone": "ES",
+    "start": "2015-01-01T00:00:00.000Z",
+    "end": "2017-01-01T00:00:00.000Z",
+    "temporalGranularity": "yearly"
+}
+params4 = {
+    "zone": "ES",
+    "start": "2017-01-01T00:00:00.000Z",
+    "end": "2025-01-01T00:00:00.000Z",
+    "temporalGranularity": "yearly"
+}
+
 response1 = requests.get(url, headers=headers, params=params1)
 response2 = requests.get(url, headers=headers, params=params2)
+response3 = requests.get(url, headers=headers, params=params3)
+response4 = requests.get(url, headers=headers, params=params4)
 
 data1 = response1.json()
 data2 = response2.json()
+data3 = response3.json()
+data4 = response4.json()
 
 rows1 = data1["data"]
 rows2 = data2["data"]
 df1 = pd.json_normalize(rows1)
 df2 = pd.json_normalize(rows2)
+rows3 = data3["data"]
+rows4 = data4["data"]
+df3 = pd.json_normalize(rows3)
+df4 = pd.json_normalize(rows4)
 
 df1 = df1[["datetime", "value"]]
 df2 = df2[["datetime", "value"]]
+df3 = df3[["datetime", "value"]]
+df4 = df4[["datetime", "value"]]
 
-df3 = pd.merge(df1, df2, how='outer')
-print(df3)
+df12 = pd.merge(df1, df2, how='outer')
+df34 = pd.merge(df3, df4, how='outer')
 
-df3["datetime"] = pd.to_datetime(df3["datetime"])
-df3["year"] = df3["datetime"].dt.year
-df3["value"] = df3["value"]/1000000
 
-#plt.figure(figsize=(10, 6))
-df3.plot(x="year", y="value", kind = "bar")
-plt.title("France - Hydroelectricity generation evolution 2015-2024")
+df12["datetime"] = pd.to_datetime(df12["datetime"])
+df12["year"] = df12["datetime"].dt.year
+df12["value"] = df12["value"]/1000000
+df34["datetime"] = pd.to_datetime(df34["datetime"])
+df34["year"] = df34["datetime"].dt.year
+df34["value"] = df34["value"]/1000000
+
+france_values = df12.groupby("year")["value"].sum().values
+spain_values = df34.groupby("year")["value"].sum().values
+
+bar_width = 0.35
+x = range(len(df12["year"]))
+
+plt.figure(figsize=(10, 6))
+plt.bar([i - bar_width/2 for i in x], france_values, width=bar_width, label = "France", color = "blue")
+plt.bar([i + bar_width/2 for i in x], spain_values, width=bar_width, label = "Spain", color = "yellow")
+plt.title("Hydroelectricity generation evolution 2015-2024")
 plt.xlabel("Year")
 plt.ylabel("Hydroelectricity Generation (TWh/year)")
+plt.grid(axis="y", linestyle="--", alpha=0.7)
+plt.xticks(x, df12["year"])
+plt.legend()
+plt.tight_layout()
 plt.show()
